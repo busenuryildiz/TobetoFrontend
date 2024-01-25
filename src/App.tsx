e00e01501272e054b727fb2ee9bb55c9584589d2
Alican Dağıdır <alicandagidir@hotmail.com>
2024-01-26 01:32:56 +0300

    Add Calendar Page and Components

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navi from './components/navbar/Navi';
 import MainPage from './pages/mainPage/MainPage';
 import PersonalInformation from './pages/profile/editProfile/personalInformation/PersonalInformation';
+import CalendarPage from './pages/calendarPage/CalendarPage';
 
 function App() {
   return (
@@ -9,10 +10,10 @@ function App() {
       <Routes>
         <Route path= "/" element={<Navi />}/>
         <Route path="/" element={<MainPage />}/>
-        <Route path="profilim/profilimi-duzenle/kisisel-bilgilerim" element={<PersonalInformation />} />
+        <Route path="profilim/profilimi-duzenle/kisisel-bilgilerim" element={<PersonalInformation />}/>
+       <Route path="/takvim" element={<CalendarPage/>}/>
       </Routes>
     </Router>
-    
   );
 }
 
