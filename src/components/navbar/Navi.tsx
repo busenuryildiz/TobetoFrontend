d13f71f42a1d06af470192f196c65625e6866662
OnurBaha <koconurbaha@gmail.com>
2024-02-11 17:39:11 +0300

    personal info updated

--- src/components/navbar/Navi.tsx
+++ src/components/navbar/Navi.tsx
@@ -5,20 +5,20 @@ import { useDispatch } from 'react-redux'
 import { AppDispatch } from '../../store/configureStore';
 import { useNavigate } from 'react-router-dom';
 function Navi() {
-const navigate = useNavigate();
-const dispatch: AppDispatch = useDispatch();
+    const navigate = useNavigate();
+    const dispatch: AppDispatch = useDispatch();
 
-const handleLogout = () => {
-    dispatch(logout()); 
-    navigate('/')
-}
+    const handleLogout = () => {
+        dispatch(logout());
+        navigate('/')
+    }
     return (
-        <Navbar className="position-relative" expand="xxl"  bg="white">
+        <Navbar className="position-relative" expand="xxl" bg="white">
             <Container fluid>
-            <Navbar.Brand href="platform">
+                <Navbar.Brand href="platform">
                     <img
-                        src="/Assets/image/tobeto-logo.png" 
-                        alt="Tobeto Logo" 
+                        src="/Assets/image/tobeto-logo.png"
+                        alt="Tobeto Logo"
                         style={{ width: '170px', height: '35px', objectFit: 'cover', background: 'transparent' }}
                     />
                 </Navbar.Brand>
@@ -43,82 +43,82 @@ const handleLogout = () => {
                     <Nav.Link href="platform" className={`nav-link c-gray-3 ${window.location.pathname === '/platform' ? 'nav-active' : ''}`}>
                         Ana Sayfa
                     </Nav.Link>
-                    <Nav.Link href="profilim"  className={`nav-link c-gray-3 ${window.location.pathname === '/profilim' ? 'nav-active' : ''}`}>
+                    <Nav.Link href="profilim/profilimi-duzenle/kisisel-bilgilerim" className={`nav-link c-gray-3 ${window.location.pathname === '/profilim' ? 'nav-active' : ''}`}>
                         Profilim
                     </Nav.Link>
                     <Nav.Link href="degerlendirmeler" className={`nav-link c-gray-3 ${window.location.pathname === '/degerlendirmeler' ? 'nav-active' : ''}`}>
                         Değerlendirmeler
                     </Nav.Link>
-                    <Nav.Link href="platform-katalog"  className={`nav-link c-gray-3 ${window.location.pathname === '/platform-katalog' ? 'nav-active' : ''}`}>
+                    <Nav.Link href="platform-katalog" className={`nav-link c-gray-3 ${window.location.pathname === '/platform-katalog' ? 'nav-active' : ''}`}>
                         Katalog
                     </Nav.Link>
-                    <Nav.Link href="takvim"  className={`nav-link c-gray-3 ${window.location.pathname === '/takvim' ? 'nav-active' : ''}`}>
+                    <Nav.Link href="takvim" className={`nav-link c-gray-3 ${window.location.pathname === '/takvim' ? 'nav-active' : ''}`}>
                         Takvim
                     </Nav.Link>
-                    <Nav.Link href="istanbul-kodluyor"  className={`nav-link c-gray-3 ${window.location.pathname === '/istanbul-kodluyor' ? 'nav-active' : ''}`}>
+                    <Nav.Link href="istanbul-kodluyor" className={`nav-link c-gray-3 ${window.location.pathname === '/istanbul-kodluyor' ? 'nav-active' : ''}`}>
                         İstanbul Kodluyor
                     </Nav.Link>
                 </Nav>
 
                 <div className="d-none d-xxl-block">
-            <div className="d-flex justify-space-between align-items-center">
-                <div className="mx-3 align-items-center d-flex align-items-center" style={{ gap: '1em' }}>
-                    <span className="tbt-gradient"></span>
-                </div>
-                <div className="btn-group header-avatar">
-                    <button
-                        type="button"
-                        className="btn p-0 fw-normal b-r-35 text-end d-flex align-items-center"
-                        data-bs-toggle="dropdown"
-                        aria-expanded="false"
-                    >
-                        <div className="me-2">
-                            <img
-                                className="cv-pp-img rounded-circle"
-                                src="Assets/image/Avatar.png"
-                                alt=""
-                                style={{ width: '36px', height: '36px', objectFit: 'cover' }}
-                            />
+                    <div className="d-flex justify-space-between align-items-center">
+                        <div className="mx-3 align-items-center d-flex align-items-center" style={{ gap: '1em' }}>
+                            <span className="tbt-gradient"></span>
                         </div>
-                        <div className="me-3">
-                            <p className="mb-0 name">Buse Yıldız</p>
+                        <div className="btn-group header-avatar">
+                            <button
+                                type="button"
+                                className="btn p-0 fw-normal b-r-35 text-end d-flex align-items-center"
+                                data-bs-toggle="dropdown"
+                                aria-expanded="false"
+                            >
+                                <div className="me-2">
+                                    <img
+                                        className="cv-pp-img rounded-circle"
+                                        src="Assets/image/Avatar.png"
+                                        alt=""
+                                        style={{ width: '36px', height: '36px', objectFit: 'cover' }}
+                                    />
+                                </div>
+                                <div className="me-3">
+                                    <p className="mb-0 name">Buse Yıldız</p>
+                                </div>
+                                <span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+                                        <path
+                                            d="M6 9L12 15L18 9"
+                                            stroke="#828282"
+                                            strokeWidth="2"
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                        ></path>
+                                    </svg>
+                                </span>
+                            </button>
+                            <ul className="profile dropdown-menu">
+                                <li>
+                                    <button className="dropdown-item profil-dropdown" onClick={() => { }}>
+                                        Profil Bilgileri
+                                    </button>
+                                </li>
+                                <li>
+                                    <hr className="dropdown-divider" style={{ backgroundColor: 'rgb(204, 204, 204)', height: '1px' }} />
+                                </li>
+                                <li>
+                                    <hr
+                                        className="dropdown-divider"
+                                        style={{ backgroundColor: 'rgba(100, 100, 100, 0.5)', height: '1px' }}
+                                    />
+                                </li>
+                                <li>
+                                    <button className="dropdown-item profil-dropdown" onClick={handleLogout}>
+                                        Oturumu Kapat
+                                    </button>
+                                </li>
+                            </ul>
                         </div>
-                        <span>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                <path
-                                    d="M6 9L12 15L18 9"
-                                    stroke="#828282"
-                                    strokeWidth="2"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                ></path>
-                            </svg>
-                        </span>
-                    </button>
-                    <ul className="profile dropdown-menu">
-                        <li>
-                            <button className="dropdown-item profil-dropdown" onClick={() => {}}>
-                                Profil Bilgileri
-                            </button>
-                        </li>
-                        <li>
-                            <hr className="dropdown-divider" style={{ backgroundColor: 'rgb(204, 204, 204)', height: '1px' }} />
-                        </li>
-                        <li>
-                            <hr
-                                className="dropdown-divider"
-                                style={{ backgroundColor: 'rgba(100, 100, 100, 0.5)', height: '1px' }}
-                            />
-                        </li>
-                        <li>
-                            <button className="dropdown-item profil-dropdown" onClick={handleLogout}>
-                                Oturumu Kapat
-                            </button>
-                        </li>
-                    </ul>
+                    </div>
                 </div>
-            </div>
-        </div>
             </Container>
         </Navbar>
     )
--- src/pages/profile/editProfile/personalInformation/PersonalInformation.tsx
+++ src/pages/profile/editProfile/personalInformation/PersonalInformation.tsx
@@ -1,325 +1,61 @@
-/*import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import personalInformationService from '../../../../services/pages/profile/editProfile/personalInformation/personalInfoService';
-import { fetchDataStart, fetchDataSuccess, fetchDataFailure, selectPersonalInformation, addUser } from '../../../../store/slices/personalInformationSlice';
-import { AddUsersRequest } from '../../../../models/requests/Users/AddUsersRequest';
-import { AddUserAddressRequest } from '../../../../models/requests/Address/addUserAddressRequest';
-import { AddUsersResponse } from '../../../../models/responses/Users/addUsersResponse';
-
-
-const PersonalInformation = () => {
-  const dispatch = useDispatch();
-  const personalInfo = useSelector(selectPersonalInformation);
-
-  const [formData, setFormData] = useState<AddUsersRequest>({
-    firstName: personalInfo?.firstName || '',
-    lastName: personalInfo?.lastName || '',
-    address: {
-      districtId: personalInfo?.address?.districtId || null,
-      cityId: personalInfo?.address?.cityId || null,
-      countryId: personalInfo?.address?.countryId || null,
-      name: personalInfo?.address?.name || '',
-      description: personalInfo?.address?.description || '',
-    },
-    email: personalInfo?.email || '',
-    password: personalInfo?.password || '' || null,
-    nationalIdentity: personalInfo?.nationalIdentity || '',
-    birthDate: personalInfo?.birthDate || new Date(),
-    phoneNumber: personalInfo?.phoneNumber || '',
-  });
-
-  useEffect(() => {
-    // Form verilerini güncelleme
-    setFormData((prevData) => ({
-      ...prevData,
-      firstName: personalInfo?.firstName || '',
-      lastName: personalInfo?.lastName || '',
-      address: {
-        districtId: personalInfo?.address?.districtId || null,
-        cityId: personalInfo?.address?.cityId || null,
-        countryId: personalInfo?.address?.countryId || null,
-        name: personalInfo?.address?.name || '',
-        description: personalInfo?.address?.description || '',
-      },
-      email: personalInfo?.email || '',
-      password: personalInfo?.password || '',
-      nationalIdentity: personalInfo?.nationalIdentity || '',
-      birthDate: personalInfo?.birthDate || new Date(),
-      phoneNumber: personalInfo?.phoneNumber || '',
-    }));
-  }, [personalInfo]);
-
-
-  const [formError, setFormError] = useState<string | null>(null);
-
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-  
-    // Eğer doğum tarihi alanı ise ve değer bir tarih formatında değilse
-    if (name === 'birthDate' && value) {
-      // Tarihi uygun bir biçime dönüştür
-      const parsedDate = new Date(value);
-  
-      // Eğer dönüştürme başarılı değilse veya değer "Invalid Date" ise
-      if (isNaN(parsedDate.getTime()) || value === 'Invalid Date') {
-        setFormError('Geçerli bir tarih girin.'); // Hata mesajını set et
-      } else {
-        setFormError(null); // Hata mesajını sıfırla
-  
-        setFormData((prevData) => ({
-          ...prevData,
-          birthDate: parsedDate, // Doğru olan burada parsedDate'i kullanmak
-        }));
-      }
-    } else {
-      setFormData((prevData) => ({
-        ...prevData,
-        [name]: value,
-      }));
-    }
-  };
-
-
-  const handleChangeAddress = (field: string, value: string) => {
-    setFormData((prevData) => ({
-      ...prevData,
-      address: {
-        ...prevData.address,
-        [field]: value,
-      },
-    }));
-  };
-
-  const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    try {
-      const newUserResponse = await personalInformationService.add({
-        ...formData,
-      });
-      const newUser: AddUsersResponse = newUserResponse.data;
-
-      dispatch(addUser(newUser));
-
-      console.log('Yeni kullanıcı başarıyla eklendi:', newUser);
-    } catch (error) {
-      console.error('İşlem hatası:', error);
-    }
-  };
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <div className="col-12 col-md-6 mb-6">
-        <label className="input-label-text">Adınız*</label>
-        <input
-          name="firstName"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.firstName}
-          onChange={handleChange}
-          required
-        />
-
-        <label className="input-label-text">Soyadınız*</label>
-        <input
-          name="lastName"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.lastName}
-          onChange={handleChange}
-          required
-        />
-
-        <label className="input-label-text">Telefon Numaranız*</label>
-        <input
-          name="phoneNumber"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.phoneNumber}
-          onChange={handleChange}
-          required
-        />
-
-        <label className="input-label-text">Doğum Tarihiniz*</label>
-        <input
-  name="birthDate"
-  className="form-control tobeto-input"
-  type="date"
-  value={formData.birthDate instanceof Date ? formData.birthDate.toISOString().split('T')[0] : ''}
-  onChange={handleChange}
-  required
-/>
-
-        <label className="input-label-text">TC Kimlik No*</label>
-        <input
-          name="nationalIdentity"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.nationalIdentity}
-          onChange={handleChange}
-          required
-        />
-
-        <label className="input-label-text">E-Posta</label>
-        <input
-          name="email"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.email}
-          onChange={handleChange}
-        />
-
-        <label className="input-label-text">Ülke*</label>
-        <input
-          name="country"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.address.countryId || ''}
-          onChange={(e) => handleChangeAddress('countryId', e.target.value)}
-          required
-        />
-
-        <label className="input-label-text">İl*</label>
-        <input
-          name="city"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.address.cityId || ''}
-          onChange={(e) => handleChangeAddress('cityId', e.target.value)}
-          required
-        />
-
-        <label className="input-label-text">İlçe*</label>
-        <input
-          name="district"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.address.districtId || ''}
-          onChange={(e) => handleChangeAddress('districtId', e.target.value)}
-          required
-        />
-
-        <label className="input-label-text">Mahalle/Sokak</label>
-        <input
-          name="address"
-          className="form-control tobeto-input"
-          type="text"
-          value={formData.address?.name || ''}
-          onChange={(e) => handleChangeAddress('name', e.target.value)}
-        />
-
-        <button type="submit" className="btn btn-primary py-2 mb-3 d-inline-block mobil-btn">
-          Kaydet
-        </button>
-      </div>
-    </form>
-  );
-}
-
-export default PersonalInformation;
-*/import React, { useEffect, useState } from 'react';
-import { useFormik } from 'formik';
-import Navi from '../../../../components/navbar/Navi';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from '../../../../store/configureStore';
+import { login } from '../../../../store/slices/authSlice';
+import { AppDispatch } from '../../../../store/configureStore';
+import User from '../../../../models/User';
+import { setPersonalInformation, setAddresses, setCities, setCountries, setLoading, setError, updateUserInfo, updateAddress } from '../../../../store/slices/personalInformationSlice';
+import Footer from '../../../../components/footer/footer';
+import { useFormik } from 'formik';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
-import PersonalInformationService, {
-  GetListUserResponse,
-  GetListAddressResponse,
-  UpdateUserRequest,
-  CreateAddressRequest,
-  UpdateAddressRequest,
-  UpdatedUserResponse,
-} from '../../../../services/pages/profile/editProfile/personalInformation/personalInfoService';
-import {
-  setPersonalInformation,
-  setAddresses,
-  setCities,
-  setCountries,
-  setLoading,
-  setError,
-  updateUserInfo,
-  addAddress,
-  updateAddress,
-} from '../../../../store/slices/personalInformationSlice';
-import Footer from '../../../../components/footer/footer';
+import PersonalInformationService, { UpdateUserRequest, UpdateAddressRequest, UpdatedUserResponse } from '../../../../services/pages/profile/editProfile/personalInformation/personalInfoService';
+import Navi from '../../../../components/navbar/Navi';
 
 const PersonalInformation = () => {
   const dispatch = useDispatch();
-  const user = useSelector((state: RootState) => state.personalInformation.user);
+  const user = useSelector((state: RootState) => state.auth.currentUser as User);
+  const [loginData, setLoginData] = useState({
+    email: '',
+    password: '',
+  });
 
   const personalInformationService = new PersonalInformationService();
 
-  const [birthDate, setBirthDate] = useState<Date | null>(new Date());
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-
-    if (name === 'birthDate' && value) {
-      const parsedDate = new Date(value);
-
-      if (!isNaN(parsedDate.getTime())) {
-        dispatch(setError(null)); // setFormError -> dispatch(setError(null))
-        formik.setFieldValue('birthDate', parsedDate); // setFormData yerine formik.setFieldValue kullanıldı
-      } else {
-        dispatch(setError('Geçerli bir tarih girin.'));
-      }
-    } else {
-      formik.handleChange(event); // setFormData -> formik.handleChange
-    }
-  };
-
-
-  // useFormik hook'unu kullanarak formun başlangıç değerlerini ve submit fonksiyonunu tanımla
   const formik = useFormik({
     initialValues: {
-      name: user?.firstName ?? '',
-      surname: user?.lastName ?? '',
-      phoneNumber: user?.phoneNumber ?? '',
+      name: user?.firstName || '',
+      surname: user?.lastName || '',
+      phoneNumber: user?.phoneNumber || '',
       birthDate: user?.birthDate ? new Date(user.birthDate) : null,
       identifier: user?.nationalIdentity ? user.nationalIdentity.toString() : '',
-      email: user?.email ?? '',
-      country: user?.address?.country ?? '',
-      city: user?.address?.city ?? '',
-      district: user?.address?.district ?? '',
-      address: user?.address?.name ?? '',
-      description: user?.address?.description ?? '',
+      email: user?.email || '',
+      country: user?.address?.country || '',
+      city: user?.address?.city || '',
+      district: user?.address?.district || '',
+      address: user?.address?.name || '',
+      description: user?.address?.description || '',
     },
     onSubmit: async (values) => {
       try {
         dispatch(setLoading(true));
-        const formattedBirthDate = values.birthDate
-          ? new Date(values.birthDate).toISOString()
-          : null;
-        // Form geçerli değilse işlem yapma
+        const formattedBirthDate = values.birthDate ? new Date(values.birthDate).toISOString() : null;
+
         if (!formik.isValid) {
           return;
         }
 
-        // Kullanıcı bilgilerini güncelle veya ekle
-        const updatedUser = user
-          ? await personalInformationService.updateUserInformation({
-            id: user.id,
-            firstName: values.name,
-            lastName: values.surname,
-            email: values.email,
-            imagePath: '',
-            password: '',
-            nationalIdentity: values.identifier.toString(),
-            birthDate: formattedBirthDate,
-            phoneNumber: ''
-          })
-          : await personalInformationService.addUser({
-            firstName: values.name,
-            lastName: values.surname,
-            email: values.email,
-            imagePath: '',
-            password: '',
-            nationalIdentity: values.identifier.toString(),
-            birthDate: formattedBirthDate,
-            phoneNumber: values.phoneNumber,
-          });
+        const updatedUser = await personalInformationService.updateUserInformation({
+          id: user?.id || '',
+          firstName: values.name,
+          lastName: values.surname,
+          email: values.email,
+          imagePath: '',
+          password: '',
+          nationalIdentity: values.identifier.toString(),
+          birthDate: formattedBirthDate,
+          phoneNumber: values.phoneNumber,
+        });
 
         const updatedUserResponse: UpdatedUserResponse = {
           id: updatedUser.data.id,
@@ -331,100 +67,52 @@ const PersonalInformation = () => {
           phoneNumber: updatedUser.data.phoneNumber,
         };
 
-        // Kullanıcının adres bilgisini güncelle veya ekle
         const updatedAddress = await personalInformationService.updateAddress({
-          id: user?.address?.id ?? 0, // Kullanıcının adres ID'si varsa kullan, yoksa 0 olarak belirle
+          id: user?.address?.id || 0,
           userId: updatedUserResponse.id,
           districtId: parseInt(values.district, 10),
           name: values.address,
-          description: values.description, // description bilgisini ekle
-          // Diğer adres bilgilerini ekleyin
+          description: values.description,
         });
 
-        // Redux store'u güncelle
         dispatch(updateUserInfo(updatedUserResponse));
         dispatch(updateAddress(updatedAddress.data));
       } catch (error) {
-        console.error('An error occurred while saving personal information:', error);
-        dispatch(setError('An error occurred while saving personal information.'));
+        console.error('Kişisel bilgileri kaydetme hatası:', error);
+        dispatch(setError('Kişisel bilgileri kaydetme hatası.'));
       } finally {
         dispatch(setLoading(false));
       }
     },
   });
 
-  // Sayfa yüklendiğinde verileri getir
   useEffect(() => {
-    let isMounted = true;
-
-    const fetchData = async () => {
+    const autoLogin = async () => {
       try {
-        dispatch(setLoading(true));
-
-        // Kullanıcı bilgilerini getir
-        if (!user) {
-          const userResponse = await personalInformationService.getUserInformation('c3477816-e13e-412a-c9fb-08dc258602df');
-          if (isMounted) {
-            dispatch(setPersonalInformation(userResponse.data));
-          }
-        }
-
-        // Adresleri getir
-        const addressesResponse = await personalInformationService.getAllAddresses();
-        if (isMounted) {
-          dispatch(setAddresses(addressesResponse.data));
-        }
-
-        // Şehirleri getir
-        const citiesResponse = await personalInformationService.getAllCities();
-        if (isMounted) {
-          dispatch(setCities(citiesResponse.data));
-        }
-
-        // Ülkeleri getir
-        const countriesResponse = await personalInformationService.getAllCountries();
-        if (isMounted) {
-          dispatch(setCountries(countriesResponse.data));
+        if (!user && (!loginData.email || !loginData.password)) {
+          const response = await fetch('http://localhost:6280/api/login');
+          const data = await response.json();
+  
+          setLoginData({
+            email: data.email,
+            password: data.password,
+          });
+  
+          const userId = data.userId;
+  
+          const userInformationResponse = await personalInformationService.getUserInformation(userId);
+  
+          // Kullanıcı bilgileri burada kullanılabilir
+          const loggedInUser = userInformationResponse.data;
+          console.log('Kullanıcı Bilgileri:', loggedInUser);
         }
       } catch (error) {
-        console.error('An error occurred while fetching data:', error);
-        dispatch(setError('An error occurred while fetching data.'));
-      } finally {
-        if (isMounted) {
-          dispatch(setLoading(false));
-        }
+        console.error('Otomatik giriş hatası:', error);
       }
     };
-
-    if (user && formik.isValid) {
-      fetchData();
-    }
-
-    return () => {
-      isMounted = false;
-    };
-  }, [dispatch, user, formik.isValid, formik.setValues]);
-
-  // Kullanıcı bilgileri değiştiğinde formu güncelle
-  useEffect(() => {
-    if (user) {
-      formik.setValues({
-        name: user.firstName,
-        surname: user.lastName,
-        phoneNumber: user.phoneNumber,
-        birthDate: user.birthDate ?? '',
-        identifier: user.nationalIdentity.toString(),
-        email: user.email,
-        country: user.address?.country ?? '',
-        city: user.address?.city ?? '',
-        district: user.address?.district ?? '',
-        address: user.address?.name ?? '',
-        description: user.address?.description ?? '',
-      });
-    }
-  }, [user, formik]);
-
-
+  
+    autoLogin();
+  }, [dispatch, user, loginData, personalInformationService]);
   return (
     <div>
       <Navi />
