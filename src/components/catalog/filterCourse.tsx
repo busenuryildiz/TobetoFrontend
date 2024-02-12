d599a230013bfc47f7c13d17ae774aa4c7a6f1c0
busenuryildiz <busenuryildiz5337@gmail.com>
2024-02-13 02:29:23 +0300

    Add course pagination

--- src/components/catalog/filterCourse.tsx
+++ src/components/catalog/filterCourse.tsx
@@ -1,7 +1,73 @@
-import React from 'react'
+// import React, { useState, ChangeEvent } from 'react';
 
-export default function FilterCourse() {
-  return (
-    <div>filterCourse</div>
-  )
-}
+// interface Category {
+//   value: string;
+//   label: string;
+// }
+
+// interface CategoryFilterProps {
+//   categories: Category[];
+//   onChange: (value: string) => void;
+// }
+
+// const CategoryFilter: React.FC<CategoryFilterProps> = ({ categories, onChange }) => {
+//   const [selectedCategory, setSelectedCategory] = useState('');
+
+//   const handleCategoryChange = (e: ChangeEvent<HTMLSelectElement>) => {
+//     setSelectedCategory(e.target.value);
+//     onChange(e.target.value);
+//   };
+
+//   return (
+//     <div>
+//       <label>Kategori:</label>
+//       <select value={selectedCategory} onChange={handleCategoryChange}>
+//         <option value="">Tümü</option>
+//         {categories.map((category) => (
+//           <option key={category.value} value={category.value}>
+//             {category.label}
+//           </option>
+//         ))}
+//       </select>
+//     </div>
+//   );
+// };
+
+// interface FilterCourseProps {
+//   onApplyFilters: (filters: { category: string }) => void;
+// }
+
+// const FilterCourse: React.FC<FilterCourseProps> = ({ onApplyFilters }) => {
+//   const [selectedFilters, setSelectedFilters] = useState({
+//     category: '',
+//     // Diğer filtreler burada eklenebilir
+//   });
+
+//   const handleFilterChange = (filterName: string, value: string) => {
+//     setSelectedFilters({ ...selectedFilters, [filterName]: value });
+//   };
+
+//   const applyFilters = () => {
+//     onApplyFilters(selectedFilters);
+//   };
+
+//   return (
+//     <div className="col-md-4">
+//       <div className="card">
+//         <div className="card-body">
+//           <h2>Filtrele</h2>
+//           <CategoryFilter
+//             categories={[
+//               { value: '1', label: 'Ücretli Eğitimler' },
+//               { value: '0', label: 'Ücretsiz Eğitimler' },
+//             ]}
+//             onChange={(value) => handleFilterChange('category', value)}
+//           />
+//           <button className="btn btn-primary mt-3" onClick={applyFilters}>Filtrele</button>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default FilterCourse;
--- src/components/catalog/filterCourse2.tsx
+++ src/components/catalog/filterCourse2.tsx
@@ -1,232 +1,218 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Accordion, Card, Form } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Course } from './courses';
+import axios from 'axios';
+ 
+const SVG_PATH = "M5 7.5L10 12.5L15 7.5";
+const PRICE_OPTIONS = ["Tüm Eğitimler", "Ücretli Eğitimler", "Ücretsiz Eğitimler"];
+const EDUCATION_OPTIONS = ["Tüm Eğitimler", "Dijital Gelişim", "Profesyonel Gelişim"];
+const COURSE_LEVEL_OPTIONS = ["Tüm Seviyeler", "Başlangıç", "Orta", "İleri"];
 
-export default function filterCourse2() {
-  return (
-    <div>filterCourse2</div>
-  )
+interface FilterCourseProps {
+  onFilterComplete: (filteredData: Array<Course>) => void;
+  course: Course[];
 }
 
-// import React from 'react'
+const FilterCourse: React.FC<FilterCourseProps> = ({ onFilterComplete,course }) => {
+  const [courses, setCourses] = useState<Array<Course>>([]); 
+  const [priceFilter, setPriceFilter] = useState<string>('all');
+  const [educationFilter, setEducationFilter] = useState<string>('all');
+  const [courseLevelFilter, setCourseLevelFilter] = useState<string>('all');
 
-// export default function filterCourse() {
-//   return (
-//     <div>filterCourse</div>
-//   )
-// }
-// import React, { useEffect, useState } from 'react';
-// import { Accordion, Card, Form } from 'react-bootstrap';
-// import 'bootstrap/dist/css/bootstrap.min.css'; 
-// import 'bootstrap/dist/js/bootstrap.bundle.min';
-// import { Course } from './courses';
-// import axios from 'axios';
-
-// const SVG_PATH = "M5 7.5L10 12.5L15 7.5";
-// const PRICE_OPTIONS = ["Tüm Eğitimler", "Ücretli Eğitimler", "Ücretsiz Eğitimler"];
-// const EDUCATION_OPTIONS = ["Tüm Eğitimler", "Dijital Gelişim", "Profesyonel Gelişim"];
-// const COURSE_LEVEL_OPTIONS = ["Tüm Seviyeler", "Başlangıç", "Orta", "İleri"];
-
-// interface FilterCourseProps {
-//   onFilterComplete: (filteredData: Array<Course>) => void;
-//   course: Course[];
-// }
-// const FilterCourse: React.FC<FilterCourseProps> = ({ onFilterComplete,course }) => {
-//   const [courses, setCourses] = useState<Array<Course>>([]); 
-//   const [priceFilter, setPriceFilter] = useState<string>('all');
-//   const [educationFilter, setEducationFilter] = useState<string>('all');
-//   const [courseLevelFilter, setCourseLevelFilter] = useState<string>('all');
-
-//   const [isPriceOpen, setIsPriceOpen] = useState<boolean>(false);
-//   const [isEducationOpen, setIsEducationOpen] = useState<boolean>(false);
-//   const [isCourseLevelOpen, setIsCourseLevelOpen] = useState<boolean>(false);
+  const [isPriceOpen, setIsPriceOpen] = useState<boolean>(false);
+  const [isEducationOpen, setIsEducationOpen] = useState<boolean>(false);
+  const [isCourseLevelOpen, setIsCourseLevelOpen] = useState<boolean>(false);
 
 
-//   useEffect(() => {
-//     const fetchCourses = async () => {
-//       try {
-//         const response = await axios.get('http://localhost:6280/api/Courses/GetList?PageSize=20');
-//         setCourses(response.data);
-//       } catch (error) {
-//         console.error('Kursları yüklerken bir hata oluştu:', error);
-//       }
-//     };
+  useEffect(() => {
+    const fetchCourses = async () => {
+      try {
+        const response = await axios.get('http://localhost:6280/api/Courses/GetList?PageSize=20');
+        setCourses(response.data);
+      } catch (error) {
+        console.error('Kursları yüklerken bir hata oluştu:', error);
+      }
+    };
 
-//     fetchCourses();
-//   }, []);
+    fetchCourses();
+  }, []);
 
-//   const filterCourses = () => {
-//     let filteredCourses = courses;
-//     if (priceFilter !== 'all') {
-//       filteredCourses = filteredCourses.filter(course => {
-//         if (priceFilter === 'Ücretli Eğitimler') {
-//           return course.price > 0;
-//         } else if (priceFilter === 'Ücretsiz Eğitimler') {
-//           return course.price === 0;
-//         } else {
-//           return true;
-//         }
-//       });
-//     }
+  const filterCourses = () => {
+    let filteredCourses = courses;
+    if (priceFilter !== 'all') {
+      filteredCourses = filteredCourses.filter(course => {
+        if (priceFilter === 'Ücretli Eğitimler') {
+          return course.price > 0;
+        } else if (priceFilter === 'Ücretsiz Eğitimler') {
+          return course.price === 0;
+        } else {
+          return true;
+        }
+      });
+    }
 
-//     if (educationFilter !== 'all') {
-//       filteredCourses = filteredCourses.filter(course => course.category === educationFilter);
-//     }
+    if (educationFilter !== 'all') {
+      filteredCourses = filteredCourses.filter(course => course.category === educationFilter);
+    }
 
-//     if (courseLevelFilter !== 'all') {
-//       filteredCourses = filteredCourses.filter(course => course.courseLevel === courseLevelFilter);
-//     }
-//     onFilterComplete(filteredCourses);
-//   };
+    if (courseLevelFilter !== 'all') {
+      filteredCourses = filteredCourses.filter(course => course.courseLevel === courseLevelFilter);
+    }
+    onFilterComplete(filteredCourses);
+  };
 
-//   const handlePriceFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setPriceFilter(event.target.value);
-//     filterCourses();
-//   };
-//   const handleEducationFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setEducationFilter(event.target.value);
-//     filterCourses();
-//   };
+  const handlePriceFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPriceFilter(event.target.value);
+    filterCourses();
+  };
+  const handleEducationFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEducationFilter(event.target.value);
+    filterCourses();
+  };
 
-//   const handleCourseLevelFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-//     setCourseLevelFilter(event.target.value);
-//     filterCourses();
-//   };
+  const handleCourseLevelFilterChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setCourseLevelFilter(event.target.value);
+    filterCourses();
+  };
 
-//   const toggleAccordion = (accordionKey: string) => {
-//     switch (accordionKey) {
-//       case 'price':
-//         setIsPriceOpen(!isPriceOpen);
-//         break;
-//       case 'education':
-//         setIsEducationOpen(!isEducationOpen);
-//         break;
-//       case 'courseLevel':
-//         setIsCourseLevelOpen(!isCourseLevelOpen);
-//         break;
-//       // Daha fazla accordion case ekleyebilirsiniz.
-//       default:
-//         break;
-//     }
-//   };
+  const toggleAccordion = (accordionKey: string) => {
+    switch (accordionKey) {
+      case 'price':
+        setIsPriceOpen(!isPriceOpen);
+        break;
+      case 'education':
+        setIsEducationOpen(!isEducationOpen);
+        break;
+      case 'courseLevel':
+        setIsCourseLevelOpen(!isCourseLevelOpen);
+        break;
+      // Daha fazla accordion case ekleyebilirsiniz.
+      default:
+        break;
+    }
+  };
 
-//   return (
-//     <div className="col-lg-3 col-md-4 col-12 light">
-//       <div className="filter dm-none">
-//         <h2>Filtrele</h2>
-//         <hr className="mt-0" />
+  return (
+    <div className="col-lg-3 col-md-4 col-12 light">
+      <div className="filter dm-none">
+        <h2>Filtrele</h2>
+        <hr className="mt-0" />
 
-//         <button
-//           className="mb-3 w-100"
-//           type="button"
-//           style={{
-//             border: '2px solid rgb(153, 51, 255)',
-//             borderRadius: '50px',
-//             padding: '8px 16px',
-//             color: 'rgb(24, 24, 24)',
-//             fontWeight: 600,
-//             textAlign: 'left',
-//             backgroundColor: 'white',
-//           }}
-//         >
-//           <div className="w-100">
-//             <span>Bana Özel</span>
-//           </div>
-//         </button>
+        <button
+          className="mb-3 w-100"
+          type="button"
+          style={{
+            border: '2px solid rgb(153, 51, 255)',
+            borderRadius: '50px',
+            padding: '8px 16px',
+            color: 'rgb(24, 24, 24)',
+            fontWeight: 600,
+            textAlign: 'left',
+            backgroundColor: 'white',
+          }}
+        >
+          <div className="w-100">
+            <span>Bana Özel</span>
+          </div>
+        </button>
 
-//         <Accordion defaultActiveKey="0" style={{ marginBottom: '-3px' }}>
-//           <Accordion.Button onClick={() => toggleAccordion('price')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
-//             <div className="title w-100" >
-//               <span>Kategori</span>
-//               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                 <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-//               </svg>
-//             </div>
-//           </Accordion.Button>
-//           <Accordion.Collapse in={isPriceOpen} eventKey="price">
-//             <Card.Body>
-//               <Form.Group className="mb-3">
-//                 <Form.Control type="text" placeholder="Arama" onChange={handlePriceFilterChange} list='datalistOptions' />
-//                 <datalist id="datalistOptions">
-//                   {PRICE_OPTIONS.map(option => <option value={option} />)}
-//                 </datalist>
+        <Accordion defaultActiveKey="0" style={{ marginBottom: '-3px' }}>
+          <Accordion.Button onClick={() => toggleAccordion('price')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
+            <div className="title w-100" >
+              <span>Kategori</span>
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </div>
+          </Accordion.Button>
+          <Accordion.Collapse in={isPriceOpen} eventKey="price">
+            <Card.Body>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Arama" onChange={handlePriceFilterChange} list='datalistOptions' />
+                <datalist id="datalistOptions">
+                  {PRICE_OPTIONS.map(option => <option value={option} key={option} />)}
+                </datalist>
 
-//               </Form.Group>
-//               <Form.Group className="mb-3">
-//                 {PRICE_OPTIONS.map(option => (
-//                   <div>
-//                     <label>
-//                       <input type="radio" className="form-check-input" name="priceFilter" style={{ marginRight: '13px', width: '16px', height: '16px' }} value={option} checked={priceFilter === option} onChange={handlePriceFilterChange} />
-//                       {option}
-//                     </label>
-//                   </div>
-//                 ))}
-//               </Form.Group>
-//             </Card.Body>
-//           </Accordion.Collapse>
-//         </Accordion>
-//         <Accordion defaultActiveKey="0" >
-//           <Accordion.Button as={Card.Header} onClick={() => toggleAccordion('education')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
-//             <div className=" title w-100">
-//               <span>Eğitimler</span>
-//               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                 <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-//               </svg>
-//             </div>
-//           </Accordion.Button>
-//           <Accordion.Collapse in={isEducationOpen} eventKey="education">
-//             <Card.Body>
-//               <Form.Group className="mb-3">
-//                 <Form.Control type="text" placeholder="Arama" onChange={handleEducationFilterChange} list='datalistOptions2' />
-//                 <datalist id="datalistOptions2">
-//                   {EDUCATION_OPTIONS.map(option => <option value={option} />)}
-//                 </datalist>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                {PRICE_OPTIONS.map(option => (
+                  <div key={option}>
+                    <label>
+                      <input type="radio" className="form-check-input" name="priceFilter" style={{ marginRight: '13px', width: '16px', height: '16px' }} value={option} checked={priceFilter === option} onChange={handlePriceFilterChange} />
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </Form.Group>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+        <Accordion defaultActiveKey="0" >
+          <Accordion.Button as={Card.Header} onClick={() => toggleAccordion('education')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
+            <div className=" title w-100">
+              <span>Eğitimler</span>
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </div>
+          </Accordion.Button>
+          <Accordion.Collapse in={isEducationOpen} eventKey="education">
+            <Card.Body>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Arama" onChange={handleEducationFilterChange} list='datalistOptions2' />
+                <datalist id="datalistOptions2">
+                  {EDUCATION_OPTIONS.map(option => <option value={option} key={option} />)}
+                </datalist>
 
-//               </Form.Group>
-//               <Form.Group className="mb-3">
-//                 {EDUCATION_OPTIONS.map(option => (
-//                   <div>
-//                     <label>
-//                       <input type="radio" name="educationFilter" value={option} style={{ marginRight: '13px', width: '16px', height: '16px' }} checked={educationFilter === option} onChange={handleEducationFilterChange} />
-//                       {option}
-//                     </label>
-//                   </div>
-//                 ))}
-//               </Form.Group>
-//             </Card.Body>
-//           </Accordion.Collapse>
-//         </Accordion>
-//         <Accordion defaultActiveKey="0">
-//           <Accordion.Button as={Card.Header} onClick={() => toggleAccordion('courseLevel')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
-//             <div className="title w-100">
-//               <span>Kurs Seviyeleri</span>
-//               <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-//                 <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-//               </svg>
-//             </div>
-//           </Accordion.Button>
-//           <Accordion.Collapse in={isCourseLevelOpen} eventKey="courseLevel">
-//             <Card.Body>
-//               <Form.Group className="mb-3">
-//                 <Form.Control type="text" placeholder="Arama" onChange={handleCourseLevelFilterChange} list='datalistOptions3' />
-//                 <datalist id="datalistOptions3">
-//                   {COURSE_LEVEL_OPTIONS.map(option => <option value={option} />)}
-//                 </datalist>
-//               </Form.Group>
-//               <Form.Group className="mb-3">
-//                 {COURSE_LEVEL_OPTIONS.map(option => (
-//                   <div>
-//                     <label>
-//                       <input type="radio" name="courseLevelFilter" value={option} style={{ marginRight: '13px', width: '16px', height: '16px' }} checked={courseLevelFilter === option} onChange={handleCourseLevelFilterChange} />
-//                       {option}
-//                     </label>
-//                   </div>
-//                 ))}
-//               </Form.Group>
-//             </Card.Body>
-//           </Accordion.Collapse>
-//         </Accordion>
-//       </div>
-//     </div>
-//   );
-// };
+              </Form.Group>
+              <Form.Group className="mb-3">
+                {EDUCATION_OPTIONS.map(option => (
+                  <div key={option}>
+                    <label>
+                      <input type="radio" name="educationFilter" value={option} style={{ marginRight: '13px', width: '16px', height: '16px' }} checked={educationFilter === option} onChange={handleEducationFilterChange} />
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </Form.Group>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+        <Accordion defaultActiveKey="0">
+          <Accordion.Button as={Card.Header} onClick={() => toggleAccordion('courseLevel')} style={{ marginLeft: '-8px', width: '112%', padding: '10px' }}>
+            <div className="title w-100">
+              <span>Kurs Seviyeleri</span>
+              <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d={SVG_PATH} stroke="#828282" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+              </svg>
+            </div>
+          </Accordion.Button>
+          <Accordion.Collapse in={isCourseLevelOpen} eventKey="courseLevel">
+            <Card.Body>
+              <Form.Group className="mb-3">
+                <Form.Control type="text" placeholder="Arama" onChange={handleCourseLevelFilterChange} list='datalistOptions3' />
+                <datalist id="datalistOptions3">
+                  {COURSE_LEVEL_OPTIONS.map(option => <option value={option} key={option} />)}
+                </datalist>
+              </Form.Group>
+              <Form.Group className="mb-3">
+                {COURSE_LEVEL_OPTIONS.map(option => (
+                  <div key={option}>
+                    <label>
+                      <input type="radio" name="courseLevelFilter" value={option} style={{ marginRight: '13px', width: '16px', height: '16px' }} checked={courseLevelFilter === option} onChange={handleCourseLevelFilterChange} />
+                      {option}
+                    </label>
+                  </div>
+                ))}
+              </Form.Group>
+            </Card.Body>
+          </Accordion.Collapse>
+        </Accordion>
+      </div>
+    </div>
+  );
+};
 
-// export default FilterCourse;
+export default FilterCourse;
