4ba5557c9b1f98c21f2c001bb62a9ee5cbed163a
busenuryildiz <busenuryildiz5337@gmail.com>
2024-02-21 03:03:52 +0300

    update SuccesModel

--- src/pages/TobetoSuccessModel/SuccessModel1.tsx
+++ src/pages/TobetoSuccessModel/SuccessModel1.tsx
@@ -3,20 +3,30 @@ import Footer from '../../components/footer/footer';
 import Navi from '../../components/navbar/Navi';
 import axios from 'axios';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 interface Question {
+    surveyID: number;
     id: number;
     text: string;
+    type: string;
 }
 
 export default function SuccessModel1() {
+    const auth = useSelector((state: any) => state.auth);
+    const userID = auth.user.id;
     const [currentSection, setCurrentSection] = useState(1);
     const [answers, setAnswers] = useState<{ [key: string]: string }>({});
+    const [answeredQuestions, setAnsweredQuestions] = useState(0);
+    const [allQuestionsAnswered, setAllQuestionsAnswered] = useState(false);
     const [questions, setQuestions] = useState<Question[]>([]);
-    const [averages, setAverages] = useState<number[]>([]);
 
     const handleRadioChange = (questionId: any, value: any) => {
+        const isQuestionAlreadyAnswered = answers.hasOwnProperty(questionId);
         setAnswers({ ...answers, [questionId]: value });
+        if (!isQuestionAlreadyAnswered) {
+            setAnsweredQuestions(answeredQuestions + 1);
+        }
     };
 
     const handlePrevClick = () => {
@@ -30,32 +40,40 @@ export default function SuccessModel1() {
             setCurrentSection(currentSection + 1);
         }
     };
-    const handleFinishClick = () => {
+    const handleFinishClick = async () => {
+        const addSurveyAnswerRequests = Object.entries(answers).map(([questionId, answerValue]) => ({
+            userID: userID,
+            surveyID: 1, 
+            questionID: parseInt(questionId),
+            answerValue: answerValue,
+        }));
+
+        const response = await axios.post('http://localhost:6280/api/Surveys/SubmitAnswers', addSurveyAnswerRequests);
+        // Handle the response
     };
     const totalQuestionCount = 80;
-    const selectedQuestionCount = 10;
 
-    const progressWidth = (selectedQuestionCount / totalQuestionCount) * 100;
+    const progressWidth = (answeredQuestions / totalQuestionCount) * 100;
     useEffect(() => {
+        const sectionQuestions = questions.slice((currentSection - 1) * 10, currentSection * 10);
+        const allAnswered = sectionQuestions.every(question => answers.hasOwnProperty(question.id));
+        setAllQuestionsAnswered(allAnswered);
         const fetchQuestions = async () => {
             try {
                 const response = await axios.get('http://localhost:6280/api/Surveys/surveys/1');
-                const averagesResponse = await axios.get('http://localhost:6280/api/Surveys/surveyansweraverages');
                 const { surveyQuestions } = response.data;
                 const formattedQuestions = surveyQuestions.map((questionData: any) => ({
                     id: questionData.questionId,
                     text: questionData.questionText,
                 }));
                 setQuestions(formattedQuestions);
-                setAverages(averagesResponse.data);
             } catch (error) {
-                // Handle errors gracefully
                 console.error('Error fetching data:', error);
             }
         };
 
         fetchQuestions();
-    }, []);
+    }, [currentSection, questions, answers]);
 
     const sectionQuestions = questions.slice((currentSection - 1) * 10, currentSection * 10);
 
@@ -105,6 +123,7 @@ export default function SuccessModel1() {
                                                                 type="radio"
                                                                 name={question.id.toString()}
                                                                 value={value.toString()}
+                                                                checked={answers[question.id] === value.toString()}
                                                                 onChange={() => handleRadioChange(question.id, value.toString())}
                                                             />
                                                         </td>
@@ -118,7 +137,7 @@ export default function SuccessModel1() {
                             <div className="col-12 text-center mt-4">
                                 <button className="btn btn-primary d-inline-block" style={{ marginRight: "10px" }} onClick={handlePrevClick} disabled={currentSection === 1}>Geri</button>
                                 {currentSection < totalQuestionCount / 10 ? (
-                                    <button className="btn btn-primary d-inline-block" onClick={handleNextClick}>İleri</button>
+                                    <button className="btn btn-primary d-inline-block" onClick={handleNextClick} disabled={!allQuestionsAnswered}>İleri</button>
                                 ) : (
 
                                     <Link to="/profilim/degerlendirmeler/rapor/tobeto-iste-basari-modeli/1" className="btn btn-primary d-inline-block" onClick={handleFinishClick}>
